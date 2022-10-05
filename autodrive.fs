7a68a4970dfcc8feb81aea11abce6fcaa067d631
Jan Atle Ramsli <jaramsli@gmail.com>
2022-10-05 15:59:54 +0200

    Update autodrive.fs

--- autodrive.fs
+++ autodrive.fs
@@ -1,29 +1,25 @@
 
+: freeSightLeft readlaser1 200 > ;
 
-: freeSightLeft
-    readlaser1
-    100 > IF
-        -1
-    ELSE
-        0
-    THEN
-;
+: freeSightRight readlaser2 200 > ;
 
-: freeSightRight
-    readlaser2
-    100 > IF
-        -1
-    ELSE
-        0
-    THEN
-;
+: freeSightAhead readlaser0 200 > ;
 
-: freeSightAhead
-    readlaser0
-    400 > IF
-        -1
-    ELSE
-        0
-    THEN
+: drive
+    100 0 DO
+        freeSightLeft freeSightRight freeSightAhead and and IF
+            speed 200 gear_fw
+        THEN
+        freeSightLeft invert IF
+            0 right_speed
+        THEN
+        freeSightRight invert IF
+            0 left_speed
+        THEN
+        freeSightAhead invert IF
+            left_bw 200 speed 1000 ms stop
+        THEN
+    LOOP
+    stop
 ;
 
