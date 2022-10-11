319c7ee42ba56d067d74d163c6b65d22bddcb5e3
Jan Atle Ramsli <jaramsli@gmail.com>
2022-10-11 10:36:35 +0200

    Update autodrive.fs

--- autodrive.fs
+++ autodrive.fs
@@ -1,9 +1,15 @@
 
 : freeSightLeft readlaser1 v_left_speed 50 + > ;
 
+: tooCloseLeft readlaser1 100 < ;
+
 : freeSightRight readlaser2 v_right_speed 50 + > ;
 
+: tooCloseRight readlaser2 100 < ;
+
 : freeSightAhead readlaser0 v_left_speed v_right_speed 50 + + > ;
+
+: obstacleAhead readlaser0 200 < ;
 
 : turnLeft
     100 speed left_fw right_bw 100 ms
@@ -14,35 +20,45 @@
 ;
 
 : backItUp
-    150 speed gear_bw 500 ms
+
+    150 speed gear_bw 500 ms stop
 ;
 
 : avoidObstacle
-    backItUp
-    readLaser1 readLaser2 < IF \ 
-        turnRight
-    ELSE
-        turnLeft
-    THEN
+
+        obstacleAhead IF
+            backItUp
+        THEN  
+
+        tooCloseLeft IF
+            speed 150 right_bw 200 ms
+        THEN
+
+        tooCloseRight IF
+            speed 150 left_bw 200 ms
+        THEN
 ;
 
 : drive
     150 speed
-    200 0 DO
+    100 0 DO
         freeSightLeft freeSightRight freeSightAhead and and IF
-            speed 200 gear_fw
+            speed 150 gear_fw
+        ELSE
+            speed 100
         THEN
+        avoidObstacle
         freeSightLeft invert IF
             150 left_speed
             50 right_speed
+            100 ms
         THEN
         freeSightRight invert IF
             150 right_speed
             50 left_speed
+            100 ms
         THEN
-        freeSightAhead invert IF
-            avoidObstacle
-        THEN
+       
         .board
     LOOP
     stop
