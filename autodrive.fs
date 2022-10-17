09b55494f6e344c06c7015ad9bddf7f7cf7ebce4
Jan Atle Ramsli <jaramsli@gmail.com>
2022-10-17 08:12:59 +0200

    Update autodrive.fs

--- autodrive.fs
+++ autodrive.fs
@@ -1,67 +1,102 @@
 
-: freeSightLeft readlaser1 150 > ;
+300 constant far
+150 constant near
 
-: tooCloseLeft readlaser1 100 < ;
+1 constant rightservo
+2 constant leftservo
+0 constant centerservo
 
-: freeSightRight readlaser2 150 > ;
+: freeSightLeft readlaser1 far > ;
 
-: tooCloseRight readlaser2 100 < ;
+: tooCloseLeft readlaser1 near < ;
 
-: freeSightAhead readlaser0 300 > ;
+: freeSightRight readlaser2 far > ;
 
-: obstacleAhead readlaser0 200 < ;
+: tooCloseRight readlaser2 near < ;
+
+: freeSightAhead readlaser0 far > ;
+
+: obstacleAhead readlaser0 far < ;
 
 : turnLeft
-    100 speed left_fw right_bw 100 ms
+    100 speed left_fw right_bw 200 ms
 ;
 
 : turnRight
-    100 speed right_fw left_bw 100 ms
+    100 speed right_fw left_bw 200 ms
 ;
 
-: backItUp
+: backItUp ( -- )
 
-    150 speed gear_bw 500 ms stop
+    150 speed gear_bw 
+    100 0 DO
+		10 ms
+		readBoard getInputs 9 = INVERT LEAVE
+    LOOP
+    stop
 ;
+
+: findFreePath ( -- )
+
+    100 speed
+    rightservo -45 servodeg
+    leftservo 45 servodeg
+    
+	 10000 0 DO
+		readLaser1 readLaser2 > 
+		IF left_fw right_bw 
+		ELSE right_fw left_bw
+		THEN
+		freeSightAhead IF
+			LEAVE
+		THEN
+	LOOP
+	servocenter
+	left_fw right_fw
+;
+
 
 : avoidObstacle
 
         obstacleAhead IF
             backItUp
+            findFreePath
         THEN  
 
         tooCloseLeft IF
-            speed 150 right_bw 200 ms
+            150 speed left_fw right_bw 300 ms
         THEN
 
         tooCloseRight IF
-            speed 150 left_bw 200 ms
+            150 speed left_bw right_fw 300 ms
         THEN
+
+        
 ;
 
 : drive
     150 speed
     100 0 DO
         freeSightLeft freeSightRight freeSightAhead and and IF
-            speed 150 gear_fw
+            150 speed gear_fw
         ELSE
-            speed 100
+            100 speed
         THEN
 
         freeSightLeft invert IF
             150 left_speed
             50 right_speed
-            100 ms
+            300 ms
         THEN
         freeSightRight invert IF
             150 right_speed
             50 left_speed
-            100 ms
+            300 ms
         THEN
-        avoidObstacle       
+        avoidObstacle 
+        left_fw
+        right_fw      
         .board
     LOOP
     stop
 ;
-
-
